a23ae56f52670db7621e37ce40bad92e4d70c7de
fahma20 <fahmaxo@gmail.com>
2024-10-09 00:06:07 +0300

    fetch on demand assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
+    const inputform = (document.querySelector ("form"))
+    //add eventListener to an input
+    inputform.addEventListener ("submit", (event) => {
+        event.preventDefault ()
+
+        const input = document.querySelector ("input#searchByID")
+        console.log (input.value)
+        fetch (`http://localhost:3000/movies/${input.value}`)
+        .then ((resp) => resp.json ())
+        .then ((data) => {
+            const title =document.querySelector("section#movieDetails")
+            //console.log (title)
+            console.log(data.title)
+            title.textContent = data.title
+        })
+      
+    })
+};
   
-}
+
 
 document.addEventListener('DOMContentLoaded', init);
